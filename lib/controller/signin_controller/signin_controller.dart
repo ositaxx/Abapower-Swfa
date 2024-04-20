0cd338c7ef2fbcfee2b8f924d5b4979e34912cae
Aiyzihk <iumeha@yahoo.com>
2024-04-20 15:07:39 +0100

    Update signin_controller.dart

--- lib/controller/signin_controller/signin_controller.dart
+++ lib/controller/signin_controller/signin_controller.dart
@@ -14,6 +14,26 @@ class SigninController extends GetxController{
 Future<void> Signin()async{
   try{
     if(!signinformKey.currentState!.validate()) return;
+     final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: '167.99.195.7',
+        port: 3306,
+        user: 'swfuser',
+        db: 'APLEICT',
+        password: 'ApleNewSwf321'
+    ));
+
+    try {
+      // Call the stored procedure with the email parameter
+      var results = await conn.query(
+      'CALL swfgetuser("${email.text.trim()}")');
+      print(results);
+
+      // Iterate through the results and print them
+      for (var row in results) {
+        print('Email: ${row[0]}, Password: ${row[1]}, Active: ${row[2]}');
+        // Here you can set the state of your widget to display the data
+        Get.offAll(()=> SigninScreen());
+      }
   }catch(e){
 
   }
